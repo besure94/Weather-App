b91b44ca3112c72eee03569c5bd3827a185411eb
Brian Scherner <brianscherner@gmail.com>
2024-04-30 10:38:02 -0700

    Extract weather forecast data and display in UI.

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -6,7 +6,7 @@ function WeatherForecast() {
   const [weatherForecast, setWeatherForecast] = useState([]);
 
   useEffect(() => {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.REACT_APP_API_KEY}`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?&units=imperial&id=524901&appid=${process.env.REACT_APP_API_KEY}`)
       .then(response => {
         if (!response.ok) {
           throw new Error(`${response.status}: ${response.statusText}`);
@@ -16,14 +16,16 @@ function WeatherForecast() {
       })
       .then((jsonifiedResponse) => {
         setWeatherForecast(jsonifiedResponse.list)
+        console.log(jsonifiedResponse);
         setIsLoaded(true)
       })
       .catch((error) => {
-        setError(error)
+        setError(error.message)
         setIsLoaded(true)
       });
   }, [])
 
+  console.log(weatherForecast);
   if (error) {
     return <h1>Error: {error}</h1>;
   } else if (!isLoaded) {
@@ -35,7 +37,9 @@ function WeatherForecast() {
         <ul>
           {weatherForecast.map((forecast, index) =>
             <li key={index}>
-              <h3>{forecast.list}</h3>
+              <h3>Date: {forecast.dt_txt}</h3>
+              <p>Temperature: {forecast.main.temp}</p>
+              <p>Description: {forecast.weather[0].description}</p>
             </li>
           )}
         </ul>
