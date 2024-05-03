eb03afbcc0f4c4aa8423daca8a260900383bea9d
Brian Scherner <brianscherner@gmail.com>
2024-05-03 09:54:24 -0700

    Edit Error response format.

--- src/api-call/weather-api-call.js
+++ src/api-call/weather-api-call.js
@@ -2,7 +2,7 @@ const getWeather = (city) => {
   return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
     .then(response => {
       if (!response.ok) {
-        throw new Error(`${response.status}: ${response.statusText}`);
+        throw new Error(`${response.status} ${response.statusText}`);
       } else {
         return response.json();
       }
