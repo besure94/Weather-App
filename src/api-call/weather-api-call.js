a7295b2d2580e2350c1315cda54cbee19b740695
Brian Scherner <brianscherner@gmail.com>
2024-09-09 18:34:36 -0700

    Change app to display current weather for location.

--- src/api-call/weather-api-call.js
+++ src/api-call/weather-api-call.js
@@ -1,5 +1,5 @@
 const getWeather = (city) => {
-  return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
+  return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(`${response.status} ${response.statusText}`);
--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import SearchForm from './SearchForm';
 import getWeather from '../api-call/weather-api-call';
-import { format } from 'date-fns';
+
+// find better API that can handle high/low, precipitation %, and other weather info
 
 function WeatherForecast() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [weatherForecast, setWeatherForecast] = useState([]);
+  const [currentWeather, setCurrentWeather] = useState([]);
   const [city, setCity] = useState("");
-  const [selectedDay, setSelectedDay] = useState(null);
+  const [currentDate, setCurrentDate] = useState(null);
 
   useEffect(() => {
     async function getWeatherApiData() {
@@ -20,18 +21,7 @@ function WeatherForecast() {
         return;
       }
 
-      const formattedForecast = result.list.map((forecast) => ({
-        ...forecast,
-        formattedDate: convertDateFormat(forecast.dt_txt),
-        date: new Date(forecast.dt_txt).toLocaleDateString()
-      }));
-
-      const groupForecastByDay = formattedForecast.reduce((acc, forecast) => {
-        (acc[forecast.date] = acc[forecast.date] || []).push(forecast);
-        return acc;
-      }, {});
-
-      setWeatherForecast(groupForecastByDay);
+      setCurrentWeather(result);
       setIsLoaded(true);
     }
 
@@ -43,15 +33,9 @@ function WeatherForecast() {
 
   const handleFormSubmission = (formInput) => {
     const { city } = formInput;
+    const todaysDate = new Date();
     setCity(city);
-  }
-
-  const handleCardClick = (day) => {
-    setSelectedDay(selectedDay === day ? null : day);
-  };
-
-  const convertDateFormat = (date) => {
-    return format(new Date(date), 'MMMM d, yyyy h:mm a');
+    setCurrentDate(todaysDate);
   }
 
   return (
@@ -61,88 +45,29 @@ function WeatherForecast() {
       {error && <h2>Error: {error}</h2>}
       {isLoaded && (
         <React.Fragment>
+          <h4>{city}</h4>
           <br/>
-          <h2>Weather Forecast For {city}</h2>
-          <p>Each date contains the forecast in 3 hour steps.</p>
-          <br/>
-
-          <div className='forecast-container'>
-            {Object.keys(weatherForecast).map(date => (
-              <div key={date} className='card'>
-                <h3 onClick={() => handleCardClick(date)}> {date} <span>{selectedDay === date ? '-' : '+'}</span>
-                </h3>
-                {selectedDay === date && (
-                  <div className='card-details'>
-                    {weatherForecast[date].map((forecast, index) => (
-                      <div key={index}>
-                        <p>{forecast.formattedDate}</p>
-                        <p>{forecast.weather[0].description.charAt(0).toUpperCase() + forecast.weather[0].description.slice(1)}</p>
-                      </div>
-                    ))}
-                  </div>
-                )}
+          <div className='current-weather'>
+            <h3>{currentDate.toLocaleString()}</h3>
+            <br/>
+            <h3>{currentWeather.main.temp.toFixed(1)}{'\u00b0'}F</h3>
+            <h4>{currentWeather.weather[0].main}</h4>
+            <br/>
+            <div className='current-weather-details'>
+              <div className='details'>
+                <h5>Feels Like</h5>
+                <p>{currentWeather.main.feels_like}{'\u00b0'}F</p>
               </div>
-            ))}
-          </div>
-
-          {/* {weatherForecast.map((forecast, index) =>
-            <div key={index}>
-              <h3 onClick={() => handleForecastClick(index)}>{forecast.formattedDate}</h3>
-              <p>{forecast.weather[0].description.charAt(0).toUpperCase() + forecast.weather[0].description.slice(1)}</p>
-              {selectedForecastDetails === index && (
-                <div className='table-container'>
-                <div className='row justify-content-center'>
-                  <div className='col-6'>
-                    <table className='table'>
-                      <thead>
-                        <tr>
-                          <th colSpan="4">Forecast Details</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr className='forecast-details'>
-                          <th>Current Temp (Celsius)</th>
-                          <td>{((forecast.main.temp - 32) / 1.8).toFixed(1)}{'\u00b0'}
-                          <br/>
-                          Feels like {((forecast.main.feels_like - 32) / 1.8).toFixed(1)}{'\u00b0'}
-                          </td>
-                        </tr>
-                        <tr className='forecast-details'>
-                          <th>Current Temp (Fahrenheit)</th>
-                          <td>{forecast.main.temp.toFixed(1)}{'\u00b0'}
-                          <br/>
-                          Feels like {forecast.main.feels_like.toFixed(1)}{'\u00b0'}
-                          </td>
-                        </tr>
-                        <tr className='forecast-details'>
-                          <th>High/Low (Celsius)</th>
-                          <td>{((forecast.main.temp_max - 32) / 1.8).toFixed(1)}{'\u00b0'}/{((forecast.main.temp_min - 32) / 1.8).toFixed(1)}{'\u00b0'}</td>
-                        </tr>
-                        <tr className='forecast-details'>
-                          <th>High/Low (Fahrenheit)</th>
-                          <td>{forecast.main.temp_max.toFixed(1)}{'\u00b0'}/{forecast.main.temp_min.toFixed(1)}{'\u00b0'}</td>
-                        </tr>
-                        <tr className='forecast-details'>
-                          <th>Humidity</th>
-                          <td>{forecast.main.humidity}%</td>
-                        </tr>
-                        <tr className='forecast-details'>
-                          <th>Wind speed (kmph)</th>
-                          <td>{(forecast.wind.speed * 1.609).toFixed(1)}</td>
-                        </tr>
-                        <tr className='forecast-details'>
-                          <th>Wind speed (mph)</th>
-                          <td>{forecast.wind.speed.toFixed(1)}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
+              <div className='details'>
+                <h5>Humidity</h5>
+                <p>{currentWeather.main.humidity}%</p>
               </div>
-              )}
-              <hr/>
+              <div className='details'>
+                <h5>Wind</h5>
+                <p>{currentWeather.wind.speed}mph</p>
+              </div>
             </div>
-          )} */}
+          </div>
         </React.Fragment>
       )}
     </div>
