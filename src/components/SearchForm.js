a9e03ed530b71f94c93ab2fe094493d21b389577
Brian Scherner <brianscherner@gmail.com>
2024-09-10 20:35:28 -0700

    Install material UI library and align/organize weather data to left of page.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import SearchIcon from '@mui/icons-material/Search';
 
 function SearchForm(props) {
 
--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -37,7 +37,7 @@ function WeatherForecast() {
   }
 
   const convertDateFormat = (date) => {
-    return format(new Date(date), 'M/dd/yyyy h:mm a');
+    return format(new Date(date), 'M/d/yyyy h:mm a');
   }
 
   return (
@@ -50,12 +50,55 @@ function WeatherForecast() {
           <h4>{currentWeather.location.name}, {currentWeather.location.region}, {currentWeather.location.country}</h4>
           <br/>
           <div className='current-weather'>
-            <h3>{localTime}</h3>
-            <br/>
-            <h3>{currentWeather.current.temp_f}{'\u00b0'}F</h3>
-            <h4>{currentWeather.current.condition.text}</h4>
-            <br/>
-            <div className='current-weather-details'>
+            <div className='location-weather'>
+              <div className='current-conditions'>
+                <div className='temp-and-conditions'>
+                  <h5 className='now-header'>Now</h5>
+                  <h3>{currentWeather.current.temp_f}{'\u00b0'}F</h3>
+                  {/* <h4>{currentWeather.current.condition.text}</h4> */}
+                </div>
+                <div className='weather-icon'>
+                  {/* <img src={currentWeather.current.condition.icon}/> */}
+                  <img src={currentWeather.forecast.forecastday[0].day.condition.icon}/>
+                </div>
+              </div>
+              <div className='high-and-low'>
+                <h5>High: {currentWeather.forecast.forecastday[0].day.maxtemp_f}</h5>
+                <h5>Low: {currentWeather.forecast.forecastday[0].day.mintemp_f}</h5>
+              </div>
+              <h5>Local Time: {localTime}</h5>
+              <br/>
+              <br/>
+              <div className='current-weather-details'>
+                <div className='details'>
+                  <h5>Feels Like</h5>
+                  <p>{currentWeather.current.feelslike_f}{'\u00b0'}F</p>
+                </div>
+                <div className='details'>
+                  <h5>Humidity</h5>
+                  <p>{currentWeather.current.humidity}%</p>
+                </div>
+                <div className='details'>
+                  <h5>Wind</h5>
+                  <p>{currentWeather.current.wind_mph}mph</p>
+                </div>
+              </div>
+              <div className='current-weather-details'>
+                <div className='details'>
+                  <h5>Rain</h5>
+                  <p>{currentWeather.forecast.forecastday[0].day.daily_chance_of_rain}%</p>
+                </div>
+                <div className='details'>
+                  <h5>Snow</h5>
+                  <p>{currentWeather.forecast.forecastday[0].day.daily_chance_of_snow}%</p>
+                </div>
+                <div className='details'>
+                  <h5>Air Quality</h5>
+                  <p>{currentWeather.current.air_quality["us-epa-index"]}</p>
+                </div>
+              </div>
+            </div>
+            {/* <div className='current-weather-details'>
               <div className='details'>
                 <h5>Feels Like</h5>
                 <p>{currentWeather.current.feelslike_f}{'\u00b0'}F</p>
@@ -82,7 +125,7 @@ function WeatherForecast() {
                   <h5>Air Quality</h5>
                   <p>{currentWeather.current.air_quality["us-epa-index"]}</p>
               </div>
-            </div>
+            </div> */}
           </div>
         </React.Fragment>
       )}
