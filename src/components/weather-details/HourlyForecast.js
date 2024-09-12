5d7f81fbc558e036bc5704ed77b2b8682659d484
Brian Scherner <brianscherner@gmail.com>
2024-09-12 16:23:25 -0700

    Work-in-progress commit for displaying hourly forecast.

--- src/components/weather-details/HourlyForecast.js
+++ src/components/weather-details/HourlyForecast.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 // working on displaying hourly forecast
 function HourlyForecast(props) {
   const { twentyFourHourForecast } = props;
+  const hourByHour = twentyFourHourForecast.forecast.forecastday[0].hour;
+  console.log("24 hr: ", hourByHour);
 
   return (
     <React.Fragment>
@@ -11,10 +13,21 @@ function HourlyForecast(props) {
       <br/>
       <div className='current-weather'>
         <div className='location-weather'>
-        <div className="hourly-forecast">
-          <h5>Hourly Forecast</h5>
-
-        </div>
+        <h5>Hourly Forecast</h5>
+          <div className="hourly-forecast">
+            <React.Fragment>
+              {/* Each hour needs to show the temp, an icon, and the time - time needs to be rounded to the hour */}
+              {hourByHour.map((hour, index) =>
+                <div key={index}>
+                  <div className="hour">
+                    <h6>{hour.temp_f}{'\u00b0'}</h6>
+                    <img src={hour.condition.icon}/>
+                    <h6>{hour.time}</h6>
+                  </div>
+                </div>
+              )}
+            </React.Fragment>
+          </div>
         </div>
       </div>
     </React.Fragment>
