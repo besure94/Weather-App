4950b2ad6e2f056b762a7db738741c4468398e05
Brian Scherner <brianscherner@gmail.com>
2024-09-13 14:26:46 -0700

    Add comments and alt attribute to img.

--- src/components/weather-details/HourlyForecast.js
+++ src/components/weather-details/HourlyForecast.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// working on displaying hourly forecast
 function HourlyForecast(props) {
   const { twentyFourHourForecast } = props;
   const hourByHour = twentyFourHourForecast.forecast.forecastday[0].hour;
@@ -13,6 +12,7 @@ function HourlyForecast(props) {
       <br/>
       <div className='current-weather'>
         <div className='location-weather'>
+          {/* need to stop h5 from moving with the container */}
         <h5>Hourly Forecast</h5>
           <div className="hourly-forecast">
             <React.Fragment>
@@ -21,7 +21,7 @@ function HourlyForecast(props) {
                 <div key={index}>
                   <div className="hour">
                     <h6>{hour.temp_f}{'\u00b0'}</h6>
-                    <img src={hour.condition.icon}/>
+                    <img src={hour.condition.icon} alt="An icon symbolizing current hourly weather condition."/>
                     <h6>{hour.time}</h6>
                   </div>
                 </div>
