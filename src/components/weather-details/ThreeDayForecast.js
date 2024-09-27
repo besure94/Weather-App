1e275fce20cf4e2a49aa3cdee22b2aa1b8b2eff0
Brian Scherner <brianscherner@gmail.com>
2024-09-27 11:45:07 -0700

    Refactor date conversion to format date before rendering.

--- src/components/weather-details/ThreeDayForecast.js
+++ src/components/weather-details/ThreeDayForecast.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { changeDateToWeekday } from '../utility-fns/convert-date-format';
 
 function ThreeDayForeCast(props) {
   const { weatherApiObject } = props;
+  console.log("API object: ", weatherApiObject);
+  const [new3DayForecast, setNew3DayForecast] = useState([]);
+
+  useEffect(() => {
+    const updated3DayForecast = changeDateFormatsToWeekday(weatherApiObject.forecast.forecastday);
+    setNew3DayForecast(updated3DayForecast);
+  }, [weatherApiObject]);
+
+  const changeDateFormatsToWeekday = (threeDayForecast) => {
+    return threeDayForecast.map(day => {
+      return {
+        ...day,
+        date: changeDateToWeekday(day.date)
+      };
+    });
+  };
 
   return (
     <React.Fragment>
@@ -14,10 +30,10 @@ function ThreeDayForeCast(props) {
           <br/>
           <div className="forecast-for-each-day">
             <React.Fragment>
-              {weatherApiObject.forecast.forecastday.map((forecast, index) =>
+              {new3DayForecast.map((forecast, index) =>
                 <div key={index}>
                   <div className="day">
-                    <h5>{changeDateToWeekday(forecast.date)}</h5>
+                    <h5>{forecast.date}</h5>
                     <img className="three-day-forecast-icon" src={forecast.day.condition.icon} alt="An icon showing the general forecast for the day."/>
                     <h5>{forecast.day.maxtemp_f}{'\u00b0'}/{forecast.day.mintemp_f}{'\u00b0'}</h5>
                   </div>
