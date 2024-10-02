5d8063d55a5314b6af828cb30bb748ec1c160d44
Brian Scherner <brianscherner@gmail.com>
2024-10-02 15:03:47 -0700

    Update alignment of current conditions.

--- src/components/weather-details/CurrentWeather.js
+++ src/components/weather-details/CurrentWeather.js
@@ -6,48 +6,39 @@ function CurrentWeather(props) {
 
   return (
     <React.Fragment>
-      <h4>{weatherApiObject.location.name}, {weatherApiObject.location.region}, {weatherApiObject.location.country}</h4>
-      <h5>{locationLocalTime}</h5>
+      <div className="location">
+        <h4>{weatherApiObject.location.name}, {weatherApiObject.location.region}, {weatherApiObject.location.country}</h4>
+        <h5>{locationLocalTime}</h5>
+      </div>
       <br/>
       <div className='current-weather'>
-        <div className='location-weather'>
-          <div className='current-conditions'>
-            <div className='temp-and-conditions'>
-              <h4>Now</h4>
-              <h1>{weatherApiObject.current.temp_f}{'\u00b0'}F</h1>
-              <h4>Feels like {weatherApiObject.current.feelslike_f}{'\u00b0'}F</h4>
-              <br/>
-              <h5>{weatherApiObject.current.condition.text}</h5>
-              <div className='high-and-low'>
-                <h6>High: {weatherApiObject.forecast.forecastday[0].day.maxtemp_f}{'\u00b0'}</h6>
-                <h6>Low: {weatherApiObject.forecast.forecastday[0].day.mintemp_f}{'\u00b0'}</h6>
-              </div>
-            </div>
-            <div className='weather-icon'>
-              <img className="current-weather-icon" src={weatherApiObject.current.condition.icon} alt="An icon showing current weather conditions."/>
-            </div>
-            <div className="current-weather-details">
-              <div className='air-quality-and-uv'>
-                <div className='details'>
-                  <h5>Air Quality</h5>
-                  <p>{weatherApiObject.current.air_quality["us-epa-index"]}</p>
-                </div>
-                <div className='details'>
-                  <h5>UV Index</h5>
-                  <p>{weatherApiObject.current.uv}</p>
-                </div>
-              </div>
-              <div className="humidity-and-wind">
-                <div className='details'>
-                  <h5>Humidity</h5>
-                  <p>{weatherApiObject.current.humidity}%</p>
-                </div>
-                <div className='details'>
-                  <h5>Wind:</h5>
-                  <p>{weatherApiObject.current.wind_mph}mph</p>
-                </div>
-              </div>
-            </div>
+        <div className='temp-and-conditions'>
+          <img className="current-weather-icon" src={weatherApiObject.current.condition.icon} alt="An icon showing current weather conditions."/>
+          <h1>{weatherApiObject.current.temp_f}{'\u00b0'}F</h1>
+          <h3>{weatherApiObject.current.condition.text}</h3>
+          <h5>Feels like {weatherApiObject.current.feelslike_f}{'\u00b0'}F</h5>
+          {/* <br/> */}
+          <div className='high-and-low'>
+            <h6>High: {weatherApiObject.forecast.forecastday[0].day.maxtemp_f}{'\u00b0'}</h6>
+            <h6>Low: {weatherApiObject.forecast.forecastday[0].day.mintemp_f}{'\u00b0'}</h6>
+          </div>
+        </div>
+        <div className="current-weather-details">
+          <div className='details'>
+            <h5>Air Quality</h5>
+            <p>{weatherApiObject.current.air_quality["us-epa-index"]}</p>
+          </div>
+          <div className='details'>
+            <h5>UV Index</h5>
+            <p>{weatherApiObject.current.uv}</p>
+          </div>
+          <div className='details'>
+            <h5>Humidity</h5>
+            <p>{weatherApiObject.current.humidity}%</p>
+          </div>
+          <div className='details'>
+            <h5>Wind</h5>
+            <p>{weatherApiObject.current.wind_mph}mph</p>
           </div>
         </div>
       </div>
