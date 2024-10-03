d9e97a9a127858c6a640bb7438076406cc678035
Brian Scherner <brianscherner@gmail.com>
2024-10-03 13:50:21 -0700

    Render different weather details by day (will update functionality to be reusable).

--- src/components/weather-details/CurrentWeather.js
+++ src/components/weather-details/CurrentWeather.js
@@ -2,8 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // render different info for days 2 and 3
+
+// would be best to map through the 3 day forecast array and display the selected day index rather than manually selecting their indexes to display data
+
 function CurrentWeather(props) {
   const { weatherApiObject, selectedForecastDay } = props;
+  console.log(weatherApiObject);
 
   return (
     <React.Fragment>
@@ -20,10 +24,21 @@ function CurrentWeather(props) {
           </React.Fragment>
         )}
 
-        {selectedForecastDay !== 0 && (
+        {selectedForecastDay === 1 && (
           <React.Fragment>
-            {/* for testing purposes - fix later */}
-            <h5>High/Low</h5>
+            <div className="icon-and-temp">
+              <img className="current-weather-icon" src={weatherApiObject.forecast.forecastday[1].day.condition.icon} alt="An icon showing current weather conditions."/>
+              <h2>{weatherApiObject.forecast.forecastday[1].day.maxtemp_f}{'\u00b0'}/{weatherApiObject.forecast.forecastday[1].day.mintemp_f}{'\u00b0'}</h2>
+            </div>
+          </React.Fragment>
+        )}
+
+        {selectedForecastDay === 2 && (
+          <React.Fragment>
+            <div className="icon-and-temp">
+              <img className="current-weather-icon" src={weatherApiObject.forecast.forecastday[2].day.condition.icon} alt="An icon showing current weather conditions."/>
+              <h2>{weatherApiObject.forecast.forecastday[2].day.maxtemp_f}{'\u00b0'}/{weatherApiObject.forecast.forecastday[2].day.mintemp_f}{'\u00b0'}</h2>
+            </div>
           </React.Fragment>
         )}
       </div>
