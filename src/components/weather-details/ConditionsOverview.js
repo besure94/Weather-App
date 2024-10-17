fbbf03989ca8bb96a181e7089b9dcd427d5912f8
Brian Scherner <brianscherner@gmail.com>
2024-10-16 21:19:57 -0700

    Work in progress commit for displaying different weather data for specific days.

--- src/components/weather-details/ConditionsOverview.js
+++ src/components/weather-details/ConditionsOverview.js
@@ -4,26 +4,40 @@ import PropTypes from "prop-types";
 function ConditionsOverview(props) {
   const { weatherApiObject, selectedForecastDay } = props;
   const [displayedWeatherByDay, setDisplayedWeatherByDay] = useState({});
+  console.log("API response: ", weatherApiObject);
+  console.log("3 day forecast: ", weatherApiObject.forecast.forecastday);
 
+  // include wind and humidity for all days - on same line
+  // only include rain/snow if chance is higher than 20 - on its own line
   useEffect(() => {
+    const forecast = weatherApiObject.forecast.forecastday[selectedForecastDay];
     if (selectedForecastDay === 0) {
       const todaysCurrentWeather = {
         ...{},
         icon: weatherApiObject.current.condition.icon,
         temperature: weatherApiObject.current.temp_f,
         condition: weatherApiObject.current.condition.text,
-        feelsLike: weatherApiObject.current.feelslike_f
+        feelsLike: weatherApiObject.current.feelslike_f,
+        wind: weatherApiObject.current.wind_mph
+      }
+      console.log("Todays weather: ", todaysCurrentWeather);
+
+      // determining logic for displaying chance of rain/snow
+      if (forecast.day.daily_chance_of_rain >= 20) {
+
       }
       setDisplayedWeatherByDay(todaysCurrentWeather);
     } else {
-      const forecast = weatherApiObject.forecast.forecastday[selectedForecastDay];
       const futureDaysWeather = {
         ...{},
         icon: forecast.day.condition.icon,
         high: forecast.day.maxtemp_f,
         low: forecast.day.mintemp_f,
         condition: forecast.day.condition.text,
+        wind: forecast.day.maxwind_mph
       }
+      console.log("Future weather: ", futureDaysWeather);
+
       setDisplayedWeatherByDay(futureDaysWeather);
     }
   }, [weatherApiObject, selectedForecastDay]);
@@ -50,6 +64,7 @@ function ConditionsOverview(props) {
               <h2>{displayedWeatherByDay.high}{'\u00b0'}/{displayedWeatherByDay.low}{'\u00b0'}</h2>
             </div>
             <h3>{displayedWeatherByDay.condition}</h3>
+            {/* <h5>Chance of rain: </h5> */}
           </React.Fragment>
         )}
       </div>
