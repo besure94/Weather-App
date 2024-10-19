0ac67c3a3d15604e68977b353d669324420e67f5
Brian Scherner <brianscherner@gmail.com>
2024-10-18 23:43:13 -0700

    Write comments for next work session.

--- src/components/weather-details/ThreeDayForecast.js
+++ src/components/weather-details/ThreeDayForecast.js
@@ -2,20 +2,35 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 // write logic to abbreviate weekdays to 3 letters (Fri, Sat, Sun, etc)
+// can abbreviate days, but the calendar dates are getting cut off
+// need to abbreviate days while preserving calendar date
 function ThreeDayForeCast(props) {
   const { weatherApiObject, onChanging3DayDateFormats, onChangingFirstDayToSayToday, onSelectingForecastDay, selectedForecastDay} = props;
   const [threeDayForecast, setNew3DayForecast] = useState([]);
 
   useEffect(() => {
     const threeDayForecastWithNewDates = onChanging3DayDateFormats(weatherApiObject.forecast.forecastday);
-    setNew3DayForecast(threeDayForecastWithNewDates);
+    // setNew3DayForecast(threeDayForecastWithNewDates);
     console.log("3 day forecast : ", threeDayForecastWithNewDates);
 
-    const moddedFirstDayOfForecast = onChangingFirstDayToSayToday(threeDayForecastWithNewDates);
+    const abbreviatedForecastWeekdays = abbreviateDaysOfWeek(threeDayForecastWithNewDates);
+    // setNew3DayForecast(abbreviatedForecastWeekdays);
+    console.log("Abbrev weekdays: ", abbreviatedForecastWeekdays);
+
+    const moddedFirstDayOfForecast = onChangingFirstDayToSayToday(abbreviatedForecastWeekdays);
     const final3DayForecast = [moddedFirstDayOfForecast, ...threeDayForecastWithNewDates.slice(1)];
     setNew3DayForecast(final3DayForecast);
 
   }, [weatherApiObject]);
+
+  const abbreviateDaysOfWeek = (threeDayForecast) => {
+    return threeDayForecast.map(index => {
+      return {
+        ...index,
+        date: index.date.substring(0, 3)
+      };
+    });
+  };
 
   return (
     <React.Fragment>
