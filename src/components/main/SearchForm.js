d86f7b42c4f5695388a20d39e6add1c7712c9e45
Brian Scherner <brianscherner@gmail.com>
2024-10-26 18:08:25 -0700

    Successfully submit form with new TextField component (search icon still missing)

--- src/components/main/SearchForm.js
+++ src/components/main/SearchForm.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import SearchOutlined from '@mui/icons-material/SearchOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
-import { IconButton } from "@mui/material";
+import { IconButton, InputAdornment } from "@mui/material";
 
 function SearchForm(props) {
 
@@ -19,23 +19,21 @@ function SearchForm(props) {
       <div className="search-form-container">
         <div className="search-form">
           <form onSubmit={handleFormSubmission}>
-            <input
+            <TextField
               type="text"
               name="city"
               placeholder="Enter a city"
-              className="form-control"
-              required/>
-            <button className="btn btn-primary form-control" type="submit">Get Weather</button>
+              slotProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton type="submit">
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+            }}
+            required/>
           </form>
-        {/* <TextField
-          type="text"
-          name="city"
-          placeholder="Enter a city"
-          slotProps={
-            <SearchIcon/>
-          }
-          onSubmit={handleFormSubmission}
-          required/> */}
         </div>
       </div>
       <br/>
