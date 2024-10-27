79c43521c8c693a98dca556012bb51f19079de77
Brian Scherner <brianscherner@gmail.com>
2024-10-26 17:40:32 -0700

    Work-in-progress commit for changing form styling/functionality.

--- src/components/main/SearchForm.js
+++ src/components/main/SearchForm.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
 import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import { IconButton } from "@mui/material";
 
-// figure out why form is so wide
 function SearchForm(props) {
 
   function handleFormSubmission(event) {
@@ -14,23 +16,30 @@ function SearchForm(props) {
 
   return (
     <React.Fragment>
-      <div className="form-container">
-        <div className="row justify-content-center">
-          <div className="search-form">
-            <form onSubmit={handleFormSubmission}>
-              <input
-                type="text"
-                name="city"
-                placeholder="Enter a city"
-                className="form-control"
-                required/>
-              <br/>
-              <button className="btn btn-primary form-control" type="submit">Get Weather</button>
-            </form>
-          </div>
+      <div className="search-form-container">
+        <div className="search-form">
+          <form onSubmit={handleFormSubmission}>
+            <input
+              type="text"
+              name="city"
+              placeholder="Enter a city"
+              className="form-control"
+              required/>
+            <button className="btn btn-primary form-control" type="submit">Get Weather</button>
+          </form>
+        {/* <TextField
+          type="text"
+          name="city"
+          placeholder="Enter a city"
+          slotProps={
+            <SearchIcon/>
+          }
+          onSubmit={handleFormSubmission}
+          required/> */}
         </div>
       </div>
-      <hr/>
+      <br/>
+      <br/>
     </React.Fragment>
   )
 }
